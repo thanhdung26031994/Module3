4c7b60033b6af42f80c328bec237f32a49a9e0d1
thanhdung26031994 <thanhdung26031994@gmail.com>
2024-01-17 11:14:14 +0700

    bai tap 1

--- ss2_design_database/library.sql
+++ ss2_design_database/library.sql
@@ -2,40 +2,44 @@ drop database if exists library;
 create database if not exists library;
 use library;
 
-create table students(
-	s_id int primary key auto_increment,
-    s_name varchar(100),
-    birthday date,
-    class_name varchar(50)
+CREATE TABLE students (
+    s_id INT PRIMARY KEY AUTO_INCREMENT,
+    s_name VARCHAR(100),
+    birthday DATE,
+    class_name VARCHAR(50)
 );
 
-create table category(
-	cate_id int primary key,
-    cate_name varchar(100)
+CREATE TABLE category (
+    cate_id INT PRIMARY KEY,
+    cate_name VARCHAR(100)
 );
 
-create table authors(
-	au_id int primary key,
-    au_name varchar(100)
+CREATE TABLE authors (
+    au_id INT PRIMARY KEY,
+    au_name VARCHAR(100)
 );
 
-create table books(
-	b_id int primary key,
-    title varchar(100),
-    page_size int check(page_size >=0),
-    cate_id int,
-    au_id int,
-    foreign key(cate_id) references category(cate_id),
-    foreign key(au_id) references authors(au_id)
+CREATE TABLE books (
+    b_id INT PRIMARY KEY,
+    title VARCHAR(100),
+    page_size INT CHECK (page_size >= 0),
+    cate_id INT,
+    au_id INT,
+    FOREIGN KEY (cate_id)
+        REFERENCES category (cate_id),
+    FOREIGN KEY (au_id)
+        REFERENCES authors (au_id)
 );
 
-create table borrows(
-	s_id int,
-    b_id int,
-    borrow_date date,
-    return_date date,
-    primary key(s_id, b_id),
-    foreign key(s_id) references students(s_id),
-    foreign key(b_id) references books(b_id)
+CREATE TABLE borrows (
+    s_id INT,
+    b_id INT,
+    borrow_date DATE,
+    return_date DATE,
+    PRIMARY KEY (s_id , b_id),
+    FOREIGN KEY (s_id)
+        REFERENCES students (s_id),
+    FOREIGN KEY (b_id)
+        REFERENCES books (b_id)
 );
 
